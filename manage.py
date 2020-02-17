ed8bb32326aece9af2c040b672052dbcbaab3bf6
seifElislam <seif.eleslam1990@gmail.com>
2020-02-17 08:38:17 +0200

    Call right settings file depends on stage env var

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza.settings')
+    stage = os.environ.get('STAGE', 'dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza.settings.' + stage.lower())
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
