1ad87c766886f5e1b840bf28e6913f8fe0d84c47
seifElislam <seif.eleslam1990@gmail.com>
2020-02-17 11:07:22 +0200

    Register models into admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+"""
+Admin Site models
+"""
 
-# Register your models here.
+from django.contrib import admin
+from core.models import Pizza, PizzaType, PizzaSize, Customer, Order
+
+admin.site.register(PizzaSize)
+admin.site.register(PizzaType)
+admin.site.register(Pizza)
+admin.site.register(Customer)
+admin.site.register(Order)
