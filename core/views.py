0c10f4cedb8121ab719241f1c0cae73bdbfa4069
seifElislam <seif.eleslam1990@gmail.com>
2020-02-17 11:01:58 +0200

    Add views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+"""
+views module
+"""
+from rest_framework import viewsets
+from core.models import PizzaSize, PizzaType, Pizza, Customer, Order
+from core.serializer import PizzaTypeSerializer, PizzaSizeSerializer, PizzaSerializer, CustomerSerializer, \
+    OrderSerializer
 
-# Create your views here.
+
+class PizzaSizeViewSet(viewsets.ModelViewSet):
+    """
+
+    """
+    queryset = PizzaSize.objects.all()
+    serializer_class = PizzaSizeSerializer
+
+
+class PizzaTypeViewSet(viewsets.ModelViewSet):
+    """
+
+    """
+    queryset = PizzaType.objects.all()
+    serializer_class = PizzaTypeSerializer
+
+
+class PizzaViewSet(viewsets.ModelViewSet):
+    """
+
+    """
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
