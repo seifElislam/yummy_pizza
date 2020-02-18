bae5ded9cfba5ab7c559591ba6cb95b4d0968c5f
seifElislam <seif.eleslam1990@gmail.com>
2020-02-18 07:25:54 +0200

    Add create/upadate function for order serializer + upate function to pizza serializer

--- core/serializer.py
+++ core/serializer.py
@@ -6,6 +6,13 @@ from rest_framework.validators import UniqueValidator
 from core.models import *
 
 
+def get_valid_ids(objects_list):
+    """
+
+    """
+    return [obj.id for obj in objects_list]
+
+
 class PizzaSizeSerializer(serializers.ModelSerializer):
     """
 
@@ -30,26 +37,46 @@ class GetPizzaSerializer(serializers.ModelSerializer):
     """
 
     """
+    id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Pizza
-        fields = ('type', 'size', 'price')
+        fields = ('id','type', 'size', 'price')
 
 
 class PizzaSerializer(serializers.ModelSerializer):
     """
 
     """
+    id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Pizza
-        fields = ('id', 'type', 'size', 'price')
+        fields = ('id', 'type', 'size', 'price', 'order')
+
+    def update(self, instance, validated_data):
+        """
+
+        :param instance:
+        :param validated_data:
+        :return:
+        """
+        if not instance.order.status:
+            instance.size = validated_data['size']
+            instance.type = validated_data['type']
+            instance.save()
+            return instance
+        raise serializers.ValidationError("This order has already been delivered")
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     """
 
     """
+    name = serializers.CharField(max_length=250, read_only=True)
+    phone_number = serializers.CharField(max_length=50, read_only=True)
+    address = serializers.CharField(max_length=250, read_only=True)
+
     class Meta:
         model = Customer
         fields = ('name', 'phone_number', 'address')
@@ -73,19 +100,21 @@ class GetOrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ('customer', 'details', 'status', 'total_price', 'created_at', 'updated_at')
+        fields = ('id', 'customer', 'details', 'status', 'total_price', 'created_at', 'updated_at')
 
 
 class OrderSerializer(serializers.ModelSerializer):
     """
 
     """
-    customer = serializers.IntegerField()
-    details = GetPizzaSerializer(many=True)
+    customer_id = serializers.IntegerField(write_only=True)
+    details = PizzaSerializer(many=True)
+    id = serializers.IntegerField(read_only=True)
+    status = serializers.BooleanField(required=False, default=False)
 
     class Meta:
         model = Order
-        fields = ('customer', 'details')
+        fields = ('id', 'status', 'customer_id', 'details')
 
     def create(self, validated_data):
         """
@@ -93,17 +122,12 @@ class OrderSerializer(serializers.ModelSerializer):
         :param validated_data:
         :return:
         """
-        customer_id = validated_data.pop('customer')
+        customer_id = validated_data.pop('customer_id')
         details_data = validated_data.pop('details')
         customer = Customer.objects.get(pk=customer_id)
-        print(customer.name)
         new_order = Order.objects.create(customer=customer)
-        print(new_order.status)
         for pizza in details_data:
-            pizzaSerializer = GetPizzaSerializer(data=pizza)
-            if pizzaSerializer.is_valid():
-                # pizzaSerializer.save()
-                Pizza.objects.create(order=new_order, type=pizza['type'], size=pizza['size'])
+            Pizza.objects.create(order=new_order, type=pizza['type'], size=pizza['size'])
         return new_order
 
     def update(self, instance, validated_data):
@@ -114,11 +138,13 @@ class OrderSerializer(serializers.ModelSerializer):
         :return:
         """
         if not instance.status:
+            print(validated_data)
+            instance.details.all().delete()
             for pizza in validated_data.get('details'):
-                pizzaSerializer = PizzaSerializer(data=pizza)
-                if pizzaSerializer.is_valid():
-                    pizzaSerializer.save()
-
+                Pizza.objects.create(order=instance, type=pizza['type'], size=pizza['size'])
+            if validated_data.get('status'):
+                instance.status = validated_data.get('status')
             instance.save()
             return instance
+        raise serializers.ValidationError("This order has already been delivered")
 
