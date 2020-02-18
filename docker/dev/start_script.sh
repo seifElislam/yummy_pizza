a015e6dcd0af73e5a60e7c051b7f89406fdc5ddb
seifElislam <seif.eleslam1990@gmail.com>
2020-02-18 08:13:00 +0200

    Load admin, pizzaTypes, pizzaSizes data when databases start

--- docker/dev/start_script.sh
+++ docker/dev/start_script.sh
@@ -6,5 +6,5 @@ pip install -r /app/requirements/dev.txt
 echo " >> starting app"
 python /app/manage.py makemigrations
 python /app/manage.py migrate
-python /app/manage.py loaddata admin.json
+python /app/manage.py loaddata fixtures.json
 python /app/manage.py runserver 0.0.0.0:8000
